e13758da987b323029680ca1e4bd59bf378df743
Bilal <bilalhankins22@gmail.com>
2021-06-21 15:03:19 -0500

    MikroOrm working with migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import mikroConfig from './db/mikro-orm.config';
+import { MikroORM } from "@mikro-orm/core";
+import { PostResolver } from './resolvers/post';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import "reflect-metadata";
 const PORT = 8080;
 const main = async () => {
   const app = express();
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
   
   app.listen(PORT, () => {
     console.log(`server started on ${PORT}`)
