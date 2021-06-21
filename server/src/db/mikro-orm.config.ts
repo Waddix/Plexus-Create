768a6dbb9030c452cc30173127a4cd9c56841eee
Bilal <bilalhankins22@gmail.com>
2021-06-21 13:41:03 -0500

    Created mikroORM config with migrations

---
+++ server/src/db/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { MikroORM } from '@mikro-orm/core';
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join(__dirname, './migrations'), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+    disableForeignKeys: false, // causes set_session_replicate error
+  },
+  entities: [],
+  dbName: 'plexus',
+  user: 'postgres',
+  password: 'postgres',
+  type: 'postgresql',
+  debug: process.env.NODE_ENV !== 'production', // shows sql
+} as Parameters<typeof MikroORM.init>[0];
