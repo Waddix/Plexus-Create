b036442ac99095ff0ac7e2c22553b01ea09cee1f
David Franks <davidfrnks7@gmail.com>
2021-06-22 11:52:44 -0500

    remove web folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/web/src');
-const DIST_DIR = path.join(__dirname, '/web/dist');
+const SRC_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.tsx`,
