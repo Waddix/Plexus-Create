fb3d46cbcf7e7ce651c9477c586f634e7839aec7
David Franks <davidfrnks7@gmail.com>
2021-06-22 12:03:04 -0500

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const SRC_DIR = path.join(__dirname, '/src');
+const SRC_DIR = path.join(__dirname, 'src/pages/');
 const DIST_DIR = path.join(__dirname, '/dist');
 
 module.exports = {
