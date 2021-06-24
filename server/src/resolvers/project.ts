9d2e9221056002a20e285aa6f953abf17085bf8f
Bilal <bilalhankins22@gmail.com>
2021-06-24 16:34:39 -0500

    Refactored Project resolver

--- server/src/resolvers/project.ts
+++ server/src/resolvers/project.ts
@@ -2,71 +2,85 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable class-methods-use-this */
 import {
-  Resolver, Query, Ctx, Arg, Mutation, Int,
+  Resolver, Query, Arg, Mutation, Field, InputType,
 } from 'type-graphql';
+import { getConnection } from 'typeorm';
 import {Project} from '../db/entities/Project';
-import { PlexusContext } from '../types';
+@InputType()
+class ProjectInput{
+  @Field(() => String)
+  title: string
 
+  @Field(() => String)
+  description: string
+
+  @Field(() => String)
+  ownerId: string
+}
 @Resolver()
 // eslint-disable-next-line import/prefer-default-export
 export class ProjectResolver {
 // ** BASIC CRUD OPERATIONS ** \\
 
   @Query(() => [Project])
-  projects(@Ctx() { em }: PlexusContext): Promise<Project[]> {
-    return em.find(Project, {});
+  projects(): Promise<Project[]> {
+    return Project.find();
   }
 
   @Query(() => Project, { nullable: true })
   project(
     @Arg('id', () => String) id: string ,
-    @Ctx() { em }: PlexusContext,
-  ): Promise<Project | null> {
-    return em.findOne(Project, { id });
+  ): Promise<Project | undefined> {
+    return Project.findOne( id );
   }
 
   @Mutation(() => Project)
+  // @UseMiddleware(auth) only loggedIn users can create/manipulate projects
+  // remove ownerId from args after session implementation
   async createProject(
-    @Arg('title', () => String) title: string,
-    @Arg('description', () => String) description: string,
-    @Arg('owner', () => Int) owner: number,
-    @Ctx() { em }: PlexusContext,
+    @Arg('input') input: ProjectInput,
+    @Arg('ownerId', () => String) ownerId: string,
   ): Promise<Project> {
-    const project = em.create(Project, { title, description, owner });
-    await em.persistAndFlush(project);
-    return project;
+    return Project.create({ ...input, ownerId }).save();
   }
-
+  
   @Mutation(() => Project, { nullable: true })
+    // @UseMiddleware(auth) only loggedIn users can create/manipulate projects
+  // remove ownerId from args after session implementation
   async updateProject(
     @Arg('id', () => String) id: string,
     @Arg('title', () => String) title: string,
     @Arg('description', () => String) description: string,
-    @Ctx() { em }: PlexusContext,
+    @Arg('ownerId', () => String) ownerId: string,
   ): Promise<Project | null> {
-    const project = await em.findOne(Project, { id });
-    if (!project) {
-      return null;
-    }
-    // if the text isnt blank
-    if (typeof title !== 'undefined' && typeof description !== 'undefined') {
-      project.title = title;
-      project.description = description;
-      await em.persistAndFlush(Project);
-    }
-    return project;
+    // const project = await Project.findOne(id);
+    // if (!project) {
+    //   return null;
+    // }
+    // // if the text isnt blank
+    // if (typeof title !== 'undefined' && typeof description !== 'undefined') {
+    //   Project.update({id}, {title, description})
+    // }
+    // return project; 
+    // using the query builder
+    const project = await getConnection()
+    .createQueryBuilder()
+    .update(Project)
+    .set({title, description})
+    .where('id = :id and "ownerId" = :ownerId', {
+      id,
+      ownerId
+    })
+    .returning("*")
+    .execute();
+    return project.raw[0];
   }
 
   @Mutation(() => Boolean)
   async deleteProject(
     @Arg('id', () => String) id: string,
-    @Ctx() { em }: PlexusContext,
   ): Promise<boolean> {
-    try {
-      await em.nativeDelete(Project, { id });
-    } catch {
-      return false;
-    }
+    await Project.delete(id)
     return true;
   }
 }
