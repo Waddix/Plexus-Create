f175ff901073aafaa64301e35cd45e484ba681a0
Bilal <bilalhankins22@gmail.com>
2021-06-26 13:38:21 -0500

    Projects page renders grid of projectCards

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import { Center, Flex } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import React from "react"
 import { ProjectCard } from "../../components/ProjectCard";
@@ -8,11 +8,11 @@ const ProjectView: React.FC<unknown> = (): JSX.Element => {
   const [{data}] = useProjectsQuery();
   // onClick to push to projects/projectID
   return (
-    <Flex m={31}>
+    <SimpleGrid columns={[2, null, 3]} spacing="20px" maxBlockSize="fit-content">
     { data?.projects.map((p) => {
-      return <ProjectCard key={p.id} description={p.description} title={p.title} createdAt={p.createdAt} updatedAt={p.updatedAt}> </ProjectCard>
+      return <ProjectCard key={p.id} id={p.id} description={p.description} title={p.title} createdAt={p.createdAt} updatedAt={p.updatedAt}> </ProjectCard>
     })}
-  </Flex>
+  </SimpleGrid>
   )
 };
 
