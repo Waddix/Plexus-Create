f148efdfbb88b1beb01c2de60a6d0c5ba8811d82
Bilal <bilalhankins22@gmail.com>
2021-06-27 21:51:14 -0500

    Util works just have to figure out the best way to combine multiple hooks

---
+++ src/utils/getProjectOwnerID.ts
@@ -0,0 +1,8 @@
+// import { useProjectQuery } from "../generated/graphql";
+
+// export const getProjectOwnerId =  (id : string) : string| undefined=> {
+//   const[{data}] =  useProjectQuery({variables:{
+//   id: parseInt(id)
+// }})
+// return data?.project?.owner.username;
+// } ;
