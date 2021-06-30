818e37a82e03cf5f5333094a039865cfa77959b1
David Franks <davidfrnks7@gmail.com>
2021-06-30 16:42:34 -0500

    Search bar rendering

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,18 @@
-import React, { Fragment } from "react";
+import { Input } from "@chakra-ui/react";
+import React, { Fragment,useState } from "react";
 
 export default function Search() {
+  const [query, setQuery] = useState("");
+
   return (
     <Fragment>
-      Hello World
+      <Input
+        type="text"
+        placeholder="Connect, Collaborate, Contribute"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={(e) => e.key === 'Enter' && alert('search triggered!')}
+      />
     </Fragment>
   )
 }
