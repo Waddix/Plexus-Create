9d49ba60bbf8cb5d0421745f9f6514674dee21bb
Bilal <bilalhankins22@gmail.com>
2021-06-29 19:09:50 -0500

    Created Tag entity

--- server/src/db/entities/Tag.ts
+++ server/src/db/entities/Tag.ts
@@ -1,21 +1,12 @@
-// /* eslint-disable import/extensions */
-// /* eslint-disable import/no-unresolved */
-// import { Field, ObjectType } from 'type-graphql';
-// import { Column, Entity, ManyToOne } from 'typeorm';
-// import { Base } from './Base';
-// import {Post} from './Post';
-
-// @ObjectType()
-// @Entity()
-// export class Tag extends Base {
-//   @Field(() => String)
-//   @Column({ type: 'text' })
-//   name!: string;
-
-//   @Field(() => [Post])
-//   @ManyToOne(() => Post, (p: Post) => p.tags)
-//   posts = Post[];
-
-// }
-
-export {}
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import { Field, ObjectType } from 'type-graphql';
+import { Column, Entity, } from 'typeorm';
+import { Base } from './Base';
+@ObjectType()
+@Entity()
+export class Tag extends Base {
+  @Field(() => String)
+  @Column({ type: 'text' })
+  name!: string;
+}
