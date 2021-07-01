a991469fe2ef3a422011038693045fb85d1f5d83
Bilal <bilalhankins22@gmail.com>
2021-07-01 17:12:43 -0500

    Generating stripe account Link properly

--- server/src/constants/stripe.ts
+++ server/src/constants/stripe.ts
@@ -3,13 +3,13 @@ import Stripe from "stripe";
 import dotenv from 'dotenv'
 dotenv.config()
 
-declare let process : {
-  env: {
-    STRIPE_SECRET: string
-  }
-}
+// declare const process : {
+//   env: {
+//     STRIPE_SECRET: string
+//   }
+// }
 
-export const stripe = new Stripe("sk_test_f4tt8qBUFUPjT2FMDSzsESoL00HR7MO6rc", {
+export const stripe = new Stripe('sk_test_f4tt8qBUFUPjT2FMDSzsESoL00HR7MO6rc', {
   apiVersion: '2020-08-27',
   typescript: true
 });
--- server/src/resolvers/stripe.ts
+++ server/src/resolvers/stripe.ts
@@ -1,30 +1,25 @@
 import Stripe from 'stripe';
 import {
-  Resolver, Mutation,
+  Resolver, Mutation, Arg,
 } from 'type-graphql';
 import { stripe } from '../constants/stripe';
 
 @Resolver()
-// eslint-disable-next-line import/prefer-default-export
-export class StripeResolver {
-  // ** BASIC CRUD OPERATIONS ** \\
 
-  // @Mutation(() => Post, { nullable: true })
-  // async updatePost(
-  //   @Arg('id', () => Int) id: number,
-  //   @Arg('text', () => String, { nullable: true }) text: string,
-  //   @Arg('type', () => String) type: string,
-  // ): Promise<Post | null> {
-  //   const post = await Post.findOne(id);
-  //   if (!post) {
-  //     return null;
-  //   }
-  //   // if the text isnt blank
-  //   if (typeof text !== 'undefined' && typeof type !== 'undefined') {
-  //     Post.update({ id }, { text, type })
-  //   }
-  //   return post;
-  // }
+export class StripeResolver {
+
+  @Mutation(() => String)
+  async createStripeLink(@Arg('stripeId', () => String) stripeId: string): Promise<Stripe.Response<Stripe.AccountLink> | string>{
+    const params: Stripe.AccountLinkCreateParams = {
+      account: stripeId,
+      refresh_url: 'http://localhost:3000/api/auth/signin',
+      return_url: 'http://localhost:3000/projects/1',
+      type: 'account_onboarding',
+    };
+    const link = await stripe.accountLinks.create(params);
+    console.log(link);
+    return link.url;
+  }
 
   @Mutation(() => String)
   async createStripeAccount(
@@ -32,10 +27,6 @@ export class StripeResolver {
   ): Promise<Stripe.Response<Stripe.Account>| string> {
     const params: Stripe.AccountCreateParams = {
       type: 'standard',
-      capabilities: {
-        card_payments: {requested: false},
-        transfers: {requested: false},
-      }
     };
     const account = await stripe.accounts.create(params);
    
