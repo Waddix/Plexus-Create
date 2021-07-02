a9f5cd040112155b1c05b971712ac6326ceb75c4
Bilal <bilalhankins22@gmail.com>
2021-07-02 13:56:03 -0500

    Fixed ID type bug; All primary keys are now GraphQL Integers

--- server/src/db/entities/Base.ts
+++ server/src/db/entities/Base.ts
@@ -1,8 +1,8 @@
-import { Field, ID, ObjectType } from 'type-graphql';
+import { Field, Int, ObjectType } from 'type-graphql';
 import { BaseEntity, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 @ObjectType({ isAbstract: true })
 export abstract class Base extends BaseEntity {
-  @Field(() => ID)
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
