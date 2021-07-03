9b7a438f336462095fc07053ec1573b17938f5f9
Bilal <bilalhankins22@gmail.com>
2021-07-02 21:39:25 -0500

    Added NumberInput Slider for donations under Project view

---
+++ src/components/projects/DonationInput.tsx
@@ -0,0 +1,33 @@
+
+import { Container, SimpleGrid, Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper } from '@chakra-ui/react'
+import React from 'react'
+import { formatDonation } from '../../utils/formatDonation'
+import {GiPayMoney} from 'react-icons/gi'
+
+
+export const CustomDonationInput: React.FC = (val)  => {
+  const [value, setValue] = React.useState(0)
+  const handleChange = (value:number | string) => setValue(value)
+  return (
+  <Container>
+      <Box>
+      <NumberInput maxW="100px" mr="2rem" value={value} onChange={handleChange}>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+        <Slider aria-label="slider-ex-4" defaultValue={value} onChangeEnd={handleChange} size="sm" step={5} focusThumbOnChange={false}>
+          <SliderTrack bg="gray.300">
+            <SliderFilledTrack bg="purple" />
+          </SliderTrack>
+          <SliderThumb boxSize={6}>
+            <Box color="orange" as={GiPayMoney} />
+          </SliderThumb>
+        </Slider>
+      </Box>
+    </Container>
+
+  );
+}
