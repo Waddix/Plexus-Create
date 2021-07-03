8ae67d2ebdb89e3553355ed5a1b1104b7e5ddd40
Bilal <bilalhankins22@gmail.com>
2021-07-03 11:32:41 -0500

    Deleted unneccessary util

--- src/utils/getProjectOwnerID.ts
+++
@@ -1,8 +0,0 @@
-// import { useProjectQuery } from "../generated/graphql";
-
-// export const getProjectOwnerId =  (id : string) : string| undefined=> {
-//   const[{data}] =  useProjectQuery({variables:{
-//   id: parseInt(id)
-// }})
-// return data?.project?.owner.username;
-// } ;
