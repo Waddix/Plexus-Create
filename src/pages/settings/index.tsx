b06f068dc653d50ea770b37176dec4172df01641
David Franks <davidfrnks7@gmail.com>
2021-07-07 10:48:10 -0500

    Settings page redirect to settings/profile

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, Fragment } from "react"
+import { useRouter } from "next/router"
+
+const Settings = () => {
+  const router = useRouter();
+
+  // Redirects user to the profile page when trying to access the base settings page.
+
+  useEffect(() => {
+    router.push('/settings/profile')
+  }, [])
+
+  return(
+    <Fragment>
+    </Fragment>
+  )
+}
+
+export default Settings
