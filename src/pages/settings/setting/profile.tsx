295c36eb19023f63fc56034c07036e2b6138ea6a
David Franks <davidfrnks7@gmail.com>
2021-07-07 17:14:15 -0500

    Styling

--- src/pages/settings/setting/profile.tsx
+++ src/pages/settings/setting/profile.tsx
@@ -12,7 +12,8 @@ import {
   Text,
   VStack,
   Button,
-  Icon
+  Icon,
+  HStack
 } from "@chakra-ui/react"
 import React, { Fragment, useContext } from "react"
 import { JsxEmit } from "typescript"
@@ -30,152 +31,158 @@ const Profile = (): JSX.Element => {
   }
 
   return (
-    <Box>
+    <VStack
+      w="100%"
+      alignContent="center"
+      align={["stretch", "center"]}
+    >
+      <Box
+        // textAlign="center"
+        width="100%"
+        d="inline-flex"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Avatar
+          name="userProfile.name"
+          src={userProfile.image}
+          size="xl"
+        />
+        <form onSubmit={handleImageUpload}>
+          <chakra.label
+            for="image"
+            cursor="pointer"
+            rounded="md"
+            fontSize="md"
+            pos="relative"
+            textAlign="center"
+          >
+            <Fragment>
+              <FormLabel
+                htmlFor="image"
+                bg="whiteAlpha.200"
+                width="max-content"
+                rounded="md"
+                px={2}
+                py={2}
+                mt={4}
+                textAlign="center"
+                _hover={{
+                  bg: useColorModeValue("orange.200", "orange.700"),
+                }}
+              >Upload a custom image
+              </FormLabel>
+              <VisuallyHidden>
+                <Input type="file" id="image" />
+              </VisuallyHidden>
+            </Fragment>
+          </chakra.label>
+        </form>
+      </Box>
+
       <Flex
+        w={["100%", "100%", "50%", "30%"]}
         justifyContent="space-between"
         alignItems="center"
       >
-        <Box
-          textAlign="center"
+        <VStack
+          alignItems="left"
+          spacing={8}
         >
-          <Avatar
-            name="userProfile.name"
-            src={userProfile.image}
-            size="xl"
-          />
-          <form onSubmit={handleImageUpload}>
-            <chakra.label
-              for="image"
-              cursor="pointer"
-              rounded="md"
-              fontSize="md"
-              pos="relative"
-            >
-              <Fragment>
-                <FormLabel
-                  htmlFor="image"
-                  bg="whiteAlpha.200"
-                  rounded="md"
-                  px={2}
-                  py={2}
-                  mt={4}
-                  _hover={{
-                    bg: useColorModeValue("orange.200", "orange.700"),
-                  }}
-                >Upload a custom image
-                </FormLabel>
-                <VisuallyHidden>
-                  <Input type="file" id="image" />
-                </VisuallyHidden>
-              </Fragment>
-
-            </chakra.label>
-          </form>
-        </Box>
-
-        <Box>
-          <VStack
-            alignItems="left"
-            spacing={8}
-            width='max-content'
+          <Flex
+            flexDirection="row"
+            justifyContent="start"
+            alignContent="center"
           >
-            <Flex
-              flexDirection="row"
-              justifyContent="start"
-              alignContent="center"
+            <Button
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('orange.200', 'orange.700'),
+              }}
+              variant="ghost"
+              px={2}
+              py={2}
+              mr={2}
+              size="sm"
+              fontSize='1rem'
             >
-              <Button
-                _hover={{
-                  textDecoration: 'none',
-                  bg: useColorModeValue('orange.200', 'orange.700'),
-                }}
-                variant="ghost"
-                px={2}
-                py={2}
-                mr={2}
-                size="sm"
-                fontSize='1rem'
-              >
-                <Icon
-                  as={FaUserEdit}
-                />
-              </Button>
-              <Heading size="md">
-                Name:
-              </Heading>
-            </Flex>
-            <Flex
-              flexDirection="row"
-              justifyContent="start"
-              alignContent="center"
-            >
-              <Button
-                _hover={{
-                  textDecoration: 'none',
-                  bg: useColorModeValue('orange.200', 'orange.700'),
-                }}
-                variant="ghost"
-                px={2}
-                py={2}
-                mr={2}
-                size="sm"
-                fontSize='1rem'
-              >
-                <Icon
-                  as={FaUserEdit}
-                />
-              </Button>
-              <Heading size="md">
-                Username:
-              </Heading>
-            </Flex>
-            <Flex
-              flexDirection="row"
-              justifyContent="start"
-              alignContent="center"
-            >
-              <Button
-                _hover={{
-                  textDecoration: 'none',
-                  bg: useColorModeValue('orange.200', 'orange.700'),
-                }}
-                variant="ghost"
-                px={2}
-                py={2}
-                mr={2}
-                size="sm"
-                fontSize='1rem'
-              >
-                <Icon
-                  as={FaUserEdit}
-                />
-              </Button>
-              <Heading size="md">
-                Title:
-              </Heading>
-            </Flex>
-          </VStack>
-        </Box>
-
-        <Box>
-          <VStack
-            alignItems="end"
-            width='max-content'
-            spacing={8}
+              <Icon
+                as={FaUserEdit}
+              />
+            </Button>
+            <Heading size="md">
+              Name:
+            </Heading>
+          </Flex>
+          <Flex
+            flexDirection="row"
+            justifyContent="start"
+            alignContent="center"
           >
-            <Text>
-              {userProfile.name}
-            </Text>
-            <Text>
-              {userProfile.username}
-            </Text>
-            <Text>
-              {userProfile.title || "No title set"}
-            </Text>
-          </VStack>
-        </Box>
+            <Button
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('orange.200', 'orange.700'),
+              }}
+              variant="ghost"
+              px={2}
+              py={2}
+              mr={2}
+              size="sm"
+              fontSize='1rem'
+            >
+              <Icon
+                as={FaUserEdit}
+              />
+            </Button>
+            <Heading size="md">
+              Username:
+            </Heading>
+          </Flex>
+          <Flex
+            flexDirection="row"
+            justifyContent="start"
+            alignContent="center"
+          >
+            <Button
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('orange.200', 'orange.700'),
+              }}
+              variant="ghost"
+              px={2}
+              py={2}
+              mr={2}
+              size="sm"
+              fontSize='1rem'
+            >
+              <Icon
+                as={FaUserEdit}
+              />
+            </Button>
+            <Heading size="md">
+              Title:
+            </Heading>
+          </Flex>
+        </VStack>
+
+        <VStack
+          alignItems="end"
+          // width='max-content'
+          spacing={8}
+        >
+          <Text>
+            {userProfile.name}
+          </Text>
+          <Text>
+            {userProfile.username}
+          </Text>
+          <Text>
+            {userProfile.title || "No title set"}
+          </Text>
+        </VStack>
       </Flex>
-    </Box>
+    </VStack>
   )
 }
 
