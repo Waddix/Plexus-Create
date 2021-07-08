3cd663c57927c4f7031bbd38a32807a5aad36cca
Bilal <bilalhankins22@gmail.com>
2021-07-08 14:40:28 -0500

    Updated Types; SelectOptions with Formik Bindings

--- src/components/forms/SelectOptions.tsx
+++ src/components/forms/SelectOptions.tsx
@@ -1,25 +1,26 @@
 /* eslint-disable react/prop-types */
+import { FormControlProps } from '@chakra-ui/react';
 import { FormControl, FormLabel, Input, FormErrorMessage, Select, SelectProps } from '@chakra-ui/react';
 import { useField } from 'formik';
-import React, { InputHTMLAttributes, ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 
 // props for useField
-type SelectOptionsProps = InputHTMLAttributes<HTMLInputElement> & {
+export type SelectOptionsProps = FormControlProps & {
   name: string;
   label: string;
   selectProps?: SelectProps;
   children: ReactNode;
 }
 // need size as part of input type def
-export const SelectOptions: React.FC<SelectOptionsProps> = ({ label, size, children, ...props }) => {
-  const [field, { error }] = useField(props);
+export const SelectOptions: React.FC<SelectOptionsProps> = ({ label, size, name, children, selectProps,}: SelectOptionsProps) => {
+  const [field, { error }] = useField(name);
   return (
     // !! converts error message to boolean 
     // '' => false
     // 'error message' => true
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Select placeholder="Select type">
+      <Select {...field} {...selectProps}placeholder="Select type">
     {children}
   </Select>
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
