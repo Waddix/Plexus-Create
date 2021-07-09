d8203783ee88663618e0c0041b89be361f21acaf
OldJamesTown <horizontaljames@gmail.com>
2021-07-09 16:53:16 -0400

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { withUrqlClient } from 'next-urql/';
-import React, { useState } from 'react';
+import React from 'react';
 // import { Fragment } from 'react';
 // import { Flex, Grid, GridItem, Stack, HStack, VStack, Box, Col } from '@chakra-ui/react';
 import { useSession } from 'next-auth/client';
-import { MainFeed } from '../components/home/MainFeed';
-import FollowingFeed from '../components/home/FollowingFeed';
+// import { MainFeed } from '../components/home/MainFeed';
+// import FollowingFeed from '../components/home/FollowingFeed';
 import Landing from '../components/home/Landing';
 import { Feed } from '../components/home/Feed';
 
