e9f40ecece049a76678a194912eefc998bc5d3d0
David Franks <davidfrnks7@gmail.com>
2021-07-09 16:34:28 -0500

    Fixed exports for unused componenets

--- src/components/home/Top10Projects.tsx
+++ src/components/home/Top10Projects.tsx
@@ -1,20 +1,22 @@
-import React, { ReactElement, useContext } from 'react'
-import { ProjectsContext } from "../../context/projectsContext"
-// import { UserContext } from '../../context/userContext'
-import Project from '../../models/project';
+// import React, { ReactElement, useContext } from 'react'
+// import { ProjectsContext } from "../../context/projectsContext"
+// // import { UserContext } from '../../context/userContext'
+// import Project from '../../models/project';
 
-function Top10Feed(): React.ReactElement {
-  const { top10Projects } = useContext(ProjectsContext);
+// function Top10Feed(): React.ReactElement {
+//   const { top10Projects } = useContext(ProjectsContext);
 
-  const projectsFeed = top10Projects.map((project: Project, i: number) => (
-    <div className="projectFeedItem" key={i}>{project.title}: {project.owner}</div>
-  ))
+//   const projectsFeed = top10Projects.map((project: Project, i: number) => (
+//     <div className="projectFeedItem" key={i}>{project.title}: {project.owner}</div>
+//   ))
 
-  return (
-    <>
-     {projectsFeed.length ? projectsFeed : 'no projects yet' }
-    </>
-  )
-}
+//   return (
+//     <>
+//       {projectsFeed.length ? projectsFeed : 'no projects yet'}
+//     </>
+//   )
+// }
 
-export default Top10Feed;
+// export default Top10Feed;
+
+export default {}
