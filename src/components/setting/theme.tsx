d0bfd0f462f8ae5650d1cd1ef7d271de12eeeca2
David Franks <davidfrnks7@gmail.com>
2021-07-10 17:46:48 -0500

    added color mode button to the theme page.

--- src/components/setting/theme.tsx
+++ src/components/setting/theme.tsx
@@ -1,16 +1,37 @@
-import { Box, Heading } from "@chakra-ui/react"
-import React, { useContext } from "react"
-import { UserContext } from "../../context/userContext"
+import {
+  Box,
+  Button,
+  useColorMode,
+  useColorModeValue
+} from "@chakra-ui/react"
+import React from "react"
+// import { UserContext } from "../../context/userContext"
 
 const Theme = (): JSX.Element => {
   // User Profile Context
-  const { userProfile } = useContext(UserContext)
+  // const { userProfile } = useContext(UserContext)
   // Conditionally render the skeleton loading effects
   // const { loadingProfile, setLoadingProfile } = useContext(UserContext)
 
+  // Color mode
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Box>
-      <Heading>{userProfile.name}'s Theme</Heading>
+    <Box
+      textAlign="center"
+      margin="auto"
+    >
+      <Button
+        size="md"
+        rounded={'md'}
+        _hover={{
+          textDecoration: 'none',
+          bg: useColorModeValue('orange.200', 'orange.700'),
+        }}
+        onClick={toggleColorMode}
+      >
+        {colorMode === "light" ? "Dark Mode" : "Light Mode"}
+      </Button>
     </Box>
   )
 }
