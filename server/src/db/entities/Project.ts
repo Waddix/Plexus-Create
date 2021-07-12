cb8033eeb361147613898d748f1ae94a4c5ac3f8
Bilal <bilalhankins22@gmail.com>
2021-07-11 22:16:33 -0500

    Removed comments

--- server/src/db/entities/Project.ts
+++ server/src/db/entities/Project.ts
@@ -2,8 +2,6 @@ import { Base } from "./Base"
 import { Field, ObjectType } from "type-graphql";
 import { Profile } from "./Profile";
 import { Column, Entity, ManyToOne, JoinTable, ManyToMany, OneToMany } from "typeorm";
-// import { FollowProject } from "./FollowProject";
-// import { Follow } from "./Follow";
 import { Tag } from "./Tag";
 import { Position } from "./Position";
 import { Post } from "./Post";
@@ -35,11 +33,6 @@ export class Project extends Base {
   @OneToMany(() => Post, (post: Post) => post.project)
   posts: Promise<Post[]>;
 
-  // // //* Allow users to follow the project by establishing jointable with Profile via Follow entity
-  // @Field()
-  // @ManyToMany(() => Profile, (profile: Profile) => profile.followedProjects)
-  // followers: Promise<Profile[]>;
-
   @Field(() => [Tag], {nullable: true})
   @ManyToMany(() => Tag, (t: Tag) => t.name, {cascade: true, nullable: true})
   @JoinTable()
@@ -49,9 +42,4 @@ export class Project extends Base {
   @OneToMany(() => Position, (p: Position) => p.project, {nullable:true})
   position: Position[];
 
-
-  // @Field(() => [Profile])
-  // @ManyToMany(() => Profile)
-  // @JoinTable()
-  // followers: Profile[];
 }
