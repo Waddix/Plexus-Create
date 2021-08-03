953c62fa4c7f2cb74a313a2068760cf5fa3a8400
David Franks <davidfrnks7@gmail.com>
2021-08-02 21:32:36 -0500

    Creating responsive boxes for the cards in the new feed to be used to replace the areas that are using the cards.

--- src/components/home/NewFeed.tsx
+++ src/components/home/NewFeed.tsx
@@ -1,9 +1,41 @@
-import React, { Fragment } from "react";
+import {
+  Box,
+  Heading
+} from "@chakra-ui/layout";
+import React from "react";
 
 const NewFeed = (): JSX.Element => {
+
   return (
-    <Fragment >
-    </Fragment>
+    <Box
+      w={["100%", "100%", "90%", "80%"]}
+      mx="auto"
+      mt={6}
+      mb="auto"
+      textAlign="center"
+    >
+      <Box
+        d={{ base: "flex", md: "none" }}
+        m="auto"
+        w="100%"
+      >
+        <Heading>Mobile View!</Heading>
+      </Box>
+      <Box
+        d={{ base: "none", md: "flex" }}
+        m="auto"
+        w="100%"
+      >
+        <Heading>{"Tablet & Desktop View!"}</Heading>
+      </Box>
+      <Box
+        d={["none", "none", "none", "none", "none", "none", "flex"]}
+        m="auto"
+        w="100%"
+      >
+        <Heading>UltraWide View!</Heading>
+      </Box>
+    </Box>
   )
 };
 
